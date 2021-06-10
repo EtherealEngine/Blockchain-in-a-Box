97cd4a098e5148e797d109897fb800dfc323435c
Hanzla Mateen <hanzlamateen@live.com>
2021-06-10 19:32:08 +0500

    Fixed createWallet endpoint

--- packages/api/src/v1/routes/auth.js
+++ packages/api/src/v1/routes/auth.js
@@ -8,6 +8,7 @@ const {
 const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
+  next();
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
