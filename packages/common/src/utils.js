c573c3bbc9c66fe7a1f39d8804b6033eb8bbf33b
Hanzla Mateen <hanzlamateen@live.com>
2021-06-10 22:36:20 +0500

    Fixed create wallet endpoint

--- packages/common/src/utils.js
+++ packages/common/src/utils.js
@@ -30,9 +30,18 @@ const readStorageHashAsBuffer = async (hash) => {
   return buffer;
 };
 
+const setCorsHeaders = res => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Expose-Headers', '*');
+  return res;
+};
+
 module.exports = {
   jsonParse,
   getExt,
   makePromise,
   readStorageHashAsBuffer,
+  setCorsHeaders,
 };
