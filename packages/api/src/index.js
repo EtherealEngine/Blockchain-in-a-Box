77d29684363c09725f178a016c1140d47d17fca7
Hanzla Mateen <hanzlamateen@live.com>
2021-06-15 16:39:32 +0500

    Added request logging for easy debugging.

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const { addV1Routes } = require("./v1/index.js");
-const { HTTP_PORT } = require("@blockchain-in-a-box/common/src/environment.js");
+const { HTTP_PORT, DEVELOPMENT } = require("@blockchain-in-a-box/common/src/environment.js");
 
 const app = express();
 
 app.use(fileUpload());
+
+let requestsLogger = (req, res, next) => {
+  let current_datetime = new Date();
+  let formatted_date =
+    current_datetime.getFullYear() +
+    "-" +
+    (current_datetime.getMonth() + 1) +
+    "-" +
+    current_datetime.getDate() +
+    " " +
+    current_datetime.getHours() +
+    ":" +
+    current_datetime.getMinutes() +
+    ":" +
+    current_datetime.getSeconds();
+  let method = req.method;
+  let url = req.url;
+  let status = res.statusCode;
+  let log = `[${formatted_date}] ${method}:${url} ${status}`;
+  console.log(log);
+  next();
+};
+
+if (DEVELOPMENT)
+  app.use(requestsLogger);
 
 addV1Routes(app);
 
