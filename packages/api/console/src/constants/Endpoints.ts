f68e4514b8713429076486045389fe30fe6c6634
Kinjal Raval <kinjalraval18591@gmail.com>
2021-11-16 17:43:58 +0530

    update api

--- packages/api/console/src/constants/Endpoints.ts
+++ packages/api/console/src/constants/Endpoints.ts
@@ -1,6 +1,6 @@
 const Endpoints = {
-  // HOST: 'http://localhost:8080',
-  HOST: 'http://af2fc18b539ee488984fa4e58de37686-1454411376.us-west-1.elb.amazonaws.com',
+   HOST: 'http://localhost:8080',
+  //HOST: 'http://af2fc18b539ee488984fa4e58de37686-1454411376.us-west-1.elb.amazonaws.com',
   GET_ADMIN_FIRSTTIME: '/api/v1/admin/firsttime',
   POST_ADMIN_LOGIN: '/api/v1/admin/login',
   POST_ADMIN_AUTHENTICATION: '/api/v1/admin/authentication',
--- packages/api/src/v1/routes/addressRoute.js
+++ packages/api/src/v1/routes/addressRoute.js
@@ -4,13 +4,17 @@ async function AddressRoutes(app){
 
 
     app.get("/api/v1/address-data", async (req,res,next)=>{
-        let networkType  = req.query.networkType
-        let email = req.query.email
-        let data = await AddressData.findOne({ where: { networkType: networkType, email: email } });
-        if(data)
-            res.end(JSON.stringify({"Status":200, "Data":data}))
-        else
+        try{
+            let networkType  = req.query.networkType
+            let email = req.query.email
+            let data = await AddressData.findOne({ where: { networkType: networkType, email: email } });
+            if(data)
+                res.end(JSON.stringify({"Status":200, "Data":data}))
+            else
+                res.end(JSON.stringify({"Status":400, "Message": "Data cannot be fetched."}))
+        }catch(err){
             res.end(JSON.stringify({"Status":400, "Message": "Data cannot be fetched."}))
+        }
     })
 
 }
